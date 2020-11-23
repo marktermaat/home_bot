986db0d3865d88ef36115947830ca4b7b46eefbe
Mark ter Maat <livestockmanagement@nedap.com>
2020-11-23 21:35:44 +0100

    Restart all bot processes when something happens

--- lib/bot/bot.ex
+++ lib/bot/bot.ex
@@ -11,6 +11,6 @@ defmodule HomeBot.Bot do
       HomeBot.Bot.MessageConsumer
     ]
 
-    Supervisor.init(children, strategy: :one_for_one)
+    Supervisor.init(children, strategy: :one_for_all)
   end
 end

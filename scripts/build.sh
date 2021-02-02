196c08fe4dcc5e232753f64fd3030b87a4227d9e
Mark ter Maat <livestockmanagement@nedap.com>
2021-02-02 11:27:57 +0100

    Updated docker version to 0.2 and fixed some release issues

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,4 +1,4 @@
 docker build -t marktermaat/home_bot -f docker/Dockerfile .
-docker tag marktermaat/home_bot marktermaat/home_bot:0.1
-docker push marktermaat/home_bot:0.1
+docker tag marktermaat/home_bot marktermaat/home_bot:0.2
+docker push marktermaat/home_bot:0.2
 docker push marktermaat/home_bot:latest
